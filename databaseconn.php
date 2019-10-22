db86acfb7316a2d96417255fcb8e8352d5ce4d0c
Victor Neerholt <victorneerholt@gmail.com>
2019-10-22 10:15:08 +0200

    har lavet en login side smat fået den til at virke

---
+++ databaseconn.php
@@ -0,0 +1,13 @@
+<?php
+$servername = 'localhost';
+$username = 'root';
+$password = '';
+$databasename = 'portfolielogin';
+
+$connect = mysqli_connect($servername, $username, $password, $databasename);
+
+if(!$connect){
+
+    die("Connectiuon failed because" .mysqli_connect_error());
+
+} 
---
+++ loginconfirm.php
@@ -0,0 +1,42 @@
+<?php 
+
+session_start();
+$hostname="localhost"; //Host navn på min database.
+$username="root"; //standard brugernavn på database for linux.
+$password=""; //adgangskode.
+$database_name="portfolielogin"; //Navnet på min SQL database.
+$tabel_name="loginform"; //Navnet på min SQL databases tabel hvor mine brugere står listet. 
+
+
+$connect = mysqli_connect($hostname, $username, $password, $database_name);
+
+
+if (!$connect){
+    die("Connection failed: " . mysqli_connect_error ());
+}
+
+
+$user = $_POST ['brugernavn'];
+
+$pass = $_POST ['adgangskode'];
+
+ 
+$sqldatabase = "SELECT * FROM $tabel_name WHERE brugernavn = '$user' && adgangskode = '$pass'";
+
+$outputresult = mysqli_query($connect, $sqldatabase);
+
+
+$count = mysqli_num_rows($outputresult);
+
+
+if($count == 1){
+
+    $_SESSION ['login'] = 1;
+
+    header("location:https://www.youtube.com/");
+
+} else {
+
+    header("location:index.html");
+
+}
