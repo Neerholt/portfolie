f11de2cf76b865093a3e56ba49866f07f0392ffa
Victor Neerholt <victorneerholt@gmail.com>
2019-10-22 21:51:04 +0200

    Har lavet en lille ændring på title

--- loggedind.php
+++ loggedind.php
@@ -7,7 +7,7 @@ if(!isset($_SESSION['login'])){
 ?>
 <html>
     <head>
-        <title>Admin Panel</title>
+        <title>Portfolie-Admin Panel</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" type="text/css" href="stylecss.css">
--- updater.php
+++ updater.php
@@ -7,7 +7,7 @@ if(!isset($_SESSION['login'])){
 ?>
 <html>
     <head>
-        <title>Updater en artikel</title>
+        <title>Portfolie-Updater Artikel</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" type="text/css" href="stylecss.css">
