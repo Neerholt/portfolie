a1e6c12f05f260d27bd0161e357c112526ac1e84
Victor Neerholt <victorneerholt@gmail.com>
2019-12-04 12:35:42 +0100

    idk kev

--- databaseconn.php
+++ databaseconn.php
@@ -1,17 +1,20 @@
 <?php
+/*
 $servername = "localhost";
 $username = "mvi01.skp-dp.sde";
 $password = "23q3z5y5";
 $databasename = "mvi01_skp_dp_sde_dk";
+ * 
+ */
 
 
-/*
+
 $servername = 'localhost';
 $username = 'root';
 $password = '';
 $databasename = 'mvi01_skp_dp_sde_dk';
 $tabel_name='loginform'; 
-*/
+
 
 $connect = mysqli_connect($servername, $username, $password, $databasename);
 
